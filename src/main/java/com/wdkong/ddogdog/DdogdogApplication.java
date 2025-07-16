261714a4aefd4759e307d4a137f8511c72cc5098
WANSU-JOO <code.wsjoo@gmail.com>
2025-07-17 00:03:24 +0900

    Initial commit

---
+++ src/main/java/com/wdkong/ddogdog/DdogdogApplication.java
@@ -0,0 +1,13 @@
+package com.wdkong.ddogdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdogdogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdogdogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wdkong/ddogdog/DdogdogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wdkong.ddogdog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdogdogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
